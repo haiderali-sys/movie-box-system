bb6174cb17401a87da5be892f47d35fb9b65f445
haiderali-sys <haiderali.sys@gmail.com>
2025-08-03 16:53:11 +0500

    done 2 methods view movies and main menu

---
+++ mbs1.py
@@ -0,0 +1,58 @@
+# function to show currect movies
+def view_movies():
+    print("THESE ARE MOVIES AVAILABLE IN OUR CINEMA")
+    for mv in show_movies:
+        print(f"Movie:{mv.name}\n Timing :{mv.timing}\n Aavailable seats :{mv.available_seats}")
+
+# functionn to book tickets
+
+def  book_ticket():
+    for mv in show_movies:
+        print(f"Movie:{mv.name}\n Timing :{mv.timing}\n Aavailable seats :{mv.available_seats}")
+        
+    user_inp_id=input("Enter movie Id:")
+
+def cancel_ticket():
+    pass
+def view_bookings():
+    pass
+def end():
+    print("thank you for using our project")
+    exit()
+# movie classs for display movies
+
+class movie:
+    def __init__(self,movie_id,name,timing,available_seats):
+        self.movie_id=movie_id
+        self.name=name
+        self.timing=timing
+        self.available_seats=available_seats
+show_movies=[
+        movie(101, 'Inception', '6 PM', 50),
+        movie(102, 'Interstellar', '9 PM', 50),
+        movie(103, 'The Dark Knight', '12 PM', 50)]
+    
+# main menu for displaying menu
+
+while True :
+    print ("==== Welcome to CineMax ====")
+    print("1. View Movies")
+    print("2. Book Ticket")
+    print("3. Cancel Ticket")
+    print("4. View Bookings")
+    print("5. Exit")
+    try:
+        user=int(input("Enter the number which option u want to visit:"))
+        if user == 1:
+            view_movies() 
+        elif user == 2:
+            book_ticket()
+        elif user == 3:
+            cancel_ticket()
+        elif user == 4:
+            view_bookings()
+        elif user ==5:
+            end()
+       
+    except ValueError:
+        print("Invalid input")
