7b0a109f547001236038590c832f03a9d79ac13c
haiderali-sys <haiderali.sys@gmail.com>
2025-08-08 21:23:21 +0500

    made the function to book a ticket

--- mbs1.py
+++ mbs1.py
@@ -2,15 +2,35 @@
 def view_movies():
     print("THESE ARE MOVIES AVAILABLE IN OUR CINEMA")
     for mv in show_movies:
-        print(f"Movie:{mv.name}\n Timing :{mv.timing}\n Aavailable seats :{mv.available_seats}")
+        print(f"Movie:{mv.name}\n Movie_Id:{mv.movie_id}\n Timing :{mv.timing}\n Aavailable seats :{mv.available_seats}")
 
 # functionn to book tickets
 
 def  book_ticket():
     for mv in show_movies:
-        print(f"Movie:{mv.name}\n Timing :{mv.timing}\n Aavailable seats :{mv.available_seats}")
-        
-    user_inp_id=input("Enter movie Id:")
+        print(f"Movie:{mv.name}\n Movie_Id:{mv.movie_id}\n Timing :{mv.timing}\n Aavailable seats :{mv.available_seats}")
+
+    while True:  
+        user_inp_id=int(input("Enter movie Id:"))
+        print ("Enter 5 to quit")
+
+        if user_inp_id==5:
+            return end()
+        found=False
+        for mv in show_movies:
+            
+            if  user_inp_id == mv.movie_id:
+                found=True
+                print(f"Name:{mv.name }\nTiming:{mv.timing}\nAvaiable seats:{mv.available_seats}")
+                if mv.available_seats>0:
+                    mv.available_seats=mv.available_seats-1
+                    return end()
+                else:
+                    print("Sorry no seats Availabale for this movie")
+                    break    
+        if not found:
+             print("You have entered wrong Movie Id")
+       
 
 def cancel_ticket():
     pass
@@ -28,7 +48,7 @@ class movie:
         self.timing=timing
         self.available_seats=available_seats
 show_movies=[
-        movie(101, 'Inception', '6 PM', 50),
+        movie(101, 'Inception', '6 PM', 0),
         movie(102, 'Interstellar', '9 PM', 50),
         movie(103, 'The Dark Knight', '12 PM', 50)]
     
